fdeb57eee71f0578992b1d2d7bb8191a138409bb
Jeremy Goossen <jeremyg995@gmail.com>
2021-11-17 02:08:15 -0600

    General cleanup.

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    // Creates an instance of QApplication
-    QApplication a(argc, argv);
+  // Creates an instance of QApplication
+  QApplication app(argc, argv);
 
-    // This is our MainWidget class containing our GUI and functionality
-    MainWidget w;
-    w.show(); // Show main window
+  // This is our MainWidget class containing our GUI and functionality
+  MainWidget widget;
+  widget.show(); // Show main window
 
-    // run the application and return execs() return value/code
-    return a.exec();
+  // run the application and return execs() return value/code
+  return app.exec();
 }
--- mainwidget.cpp
+++ mainwidget.cpp
@@ -3,46 +3,43 @@
 
 // Constructor for main window
 MainWidget::MainWidget(QWidget *parent) :
-    QWidget(parent)
-{
-   button_ = new QPushButton(tr("Push Me!"));
-   textBrowser_ = new QTextBrowser();
+    QWidget(parent) {
+  button_       = new QPushButton(tr("Push Me!"));
+  textBrowser_  = new QTextBrowser();
 
-   QGridLayout *mainLayout = new QGridLayout;
-   mainLayout->addWidget(button_,0,0);
-   mainLayout->addWidget(textBrowser_,1,0);
-   setLayout(mainLayout);
-   setWindowTitle(tr("Connecting buttons to processes.."));
+  QGridLayout *mainLayout = new QGridLayout;
+  mainLayout->addWidget(button_,0,0);
+  mainLayout->addWidget(textBrowser_,1,0);
+  setLayout(mainLayout);
+  setWindowTitle(tr("Connecting buttons to processes.."));
 
-   connect(button_, SIGNAL(released()), this, SLOT(onButtonReleased()));
-   connect(&process_, SIGNAL(readyReadStandardOutput()), this, SLOT(onCaptureProcessOutput()));
+  connect(button_, SIGNAL(released()), this, SLOT(onButtonReleased()));
+  connect(&process_, SIGNAL(readyReadStandardOutput()), this, SLOT(onCaptureProcessOutput()));
 }
 
 // Destructor
-MainWidget::~MainWidget()
-{
-    delete button_;
-   delete textBrowser_;
+MainWidget::~MainWidget() {
+  delete button_;
+  delete textBrowser_;
 }
 
 // Handler for button click
-void MainWidget::onButtonReleased()
-{
-    // clear the text in the textBrowser and start the process
-    textBrowser_->clear();
+void MainWidget::onButtonReleased() {
+  // clear the text in the textBrowser and start the process
+  textBrowser_->clear();
 
-    // Set up our process to write to stdout and run our command
-    process_.setCurrentWriteChannel(QProcess::StandardOutput); // Set the write channel
-   process_.start(QString("ls -alh ").append(QDir::homePath()).append("/Desktop")); // Start a terminal command
+  // Set up our process to write to stdout and run our command
+  process_.setCurrentWriteChannel(QProcess::StandardOutput); // Set the write channel
+  process_.start(QString("ls -alh ").append(QDir::homePath()).append("/Desktop")); // Start a terminal command
 }
 
 
 // This is called whenever the QProcess::readyReadStandardOutput() signal is received
-void MainWidget::onCaptureProcessOutput()
-{
-   // Determine whether the object that sent the signal was a pointer to a process
-   QProcess* process = qobject_cast<QProcess*>(sender());
-   // If so, append the output to the textbrowser
-   if (process)
-       textBrowser_->append(process->readAllStandardOutput());
+void MainWidget::onCaptureProcessOutput() {
+  // Determine whether the object that sent the signal was a pointer to a process
+  QProcess* process = qobject_cast<QProcess*>(sender());
+  // If so, append the output to the textbrowser
+  if (process){
+    textBrowser_->append(process->readAllStandardOutput());
+  }
 }
